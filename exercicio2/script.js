0d71d404bd7c0cdb50881183f4bc45157bb69b1e
Jessé Amaral <jesseelite286@gmail.com>
2022-07-21 21:39:54 -0300

    3 exercicios feitos da aula a008

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+const a = Number(prompt("Insira um valoe para a")) //80
+const b = Number(prompt("Insira um valoe para b")) //20
+
+console.log("O primeiro numero é maior que segundo?", (a > b))
+console.log("O primeiro numero é igual ao segundo?", (a === b ))
+console.log("O primeiro numero é divisível pelo segundo?", (a % b)===0)
+console.log("O segundo numero é divisível pelo primeiro?", (b % a)===0)
